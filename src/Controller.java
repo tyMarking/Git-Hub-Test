34cb9fb25fb7aeec761cee9252ec6ae4962af6f9
Ty Marking <30440916+tyMarking@users.noreply.github.com>
2017-08-24 16:43:58 -0700

    Added Hello World Print statement in main in Controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,8 @@
 
 public class Controller {
 	
-	public static void Main (String[] args) {
-		
+	public static void main (String[] args) {
+		System.out.println("Hello Git Hub World!");
+		System.exit(0);
 	}
 }
